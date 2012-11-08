cae4d4e094a10b51f01716e9708729b934f77a31
yashwinkumar <y_ashu_rao@yahoo.com>
2012-11-08 12:00:33 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Networkpedia::Application.config.secret_token = 'bfd15df8dbd2a018efe5c054158518cd15713386e0c38300be94099204eb537be87c4391a995abec5eceea90a70a0c0dccfb6e452be69b84a09a4254861cd308'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Networkpedia::Application.config.session_store :cookie_store, key: '_networkpedia_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Networkpedia::Application.config.session_store :active_record_store
